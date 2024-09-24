d8a46ac071f520b7d69f5c0c0bb02c809871fa82
ryanrepos <mamarasulov911@gmail.com>
2024-09-24 14:43:00 +0400

    feat: meta tags integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// Meta data larni qurish uchun ishlatamiz 
+
 export default function Document() {
   console.log("DOCUMENT - PAGER ROUTER")
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/** SEO */}
+        <meta name="keyword" content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}/>
+        <meta name="description" content={"BUY AND SELL PROPERTIES ANYWHERE ANYTIME IN SOUTH KOREA. | " + 
+        "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다" + 
+        "ПОКУПАЙТЕ И ПРОДАВАЙТЕ НЕДВИЖИМОСТЬ В ГДЕ В ЛЮБОЕ ВРЕМЯ В ЮЖНОЙ КОРЕЕ"
+      } />
+      </Head>
       <body>
         <Main />
         <NextScript />
