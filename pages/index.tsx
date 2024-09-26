e5c6b822164cb72ea22f82c4ecd74f42ec4aacc2
ryanrepos <mamarasulov911@gmail.com>
2024-09-26 11:30:06 +0400

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,20 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
+    <Stack className={"home-page"}>
+
         <Stack>
           <Stack className="container">Popular Properties</Stack>
         </Stack>
+
         <Stack>
           <Stack className="container">Top Agents</Stack>
         </Stack>
+
         <Stack>
           <Stack className="container">Top Properties</Stack>
         </Stack>
-      </Stack>
+
     </Stack>
   );
 };
